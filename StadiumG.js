59f01142f709bb4a8d99d503543019c60018ac44
Titiwat <titiwat.te06@gmail.com>
2026-04-09 14:16:24 +0700

    edited more in stadium G

--- StadiumG.js
+++ StadiumG.js
@@ -11,6 +11,16 @@ class StadiumG {
         this.ticket.push(ticket);
         console.log(`Added ${ticket.name} to the ${this.StadiumGName}`);
     }
+
+    removeTicket(ticket) {
+        const index = this.ticket.indexOf(ticket);
+        if (index !== -1) {
+            this.ticket.splice(index, 1); 
+            console.log(`Removed ${ticket.name} from ${this.StadiumGName}`);
+        } else {
+            console.log("Ticket not found!");
+        }
+    }
 }
 
 module.exports = StadiumG;
